09ccff2bcbba6e19959ca01bfab2a424f02cd17c
Juan <juan.alvarez.vaa@gmail.com>
2024-08-23 23:24:16 -0300

    Improve UI of Characters screen

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Avatar } from "@mui/material";
 import { WestOutlined } from "@mui/icons-material";
 import "./Character.css";
 
@@ -8,50 +8,53 @@ type CharacterProps = {
 };
 const Character = ({ info, onBack }: CharacterProps) => {
   return (
-    <div>
+    <div className="character">
       <div className="back_btn">
         <Button onClick={onBack} startIcon={<WestOutlined />}>
           Back
         </Button>
       </div>
-      <div className="character_image">
-        <img
-          src={
-            info.image ||
-            "https://www.svgrepo.com/show/452030/avatar-default.svg"
-          }
+      <br />
+      <div className="character_info">
+        <Avatar
+          src={info.image}
           alt={info.actor}
           className="character_img"
+          sx={{
+            width: 250,
+            height: 250,
+            border: "3px solid white",
+          }}
         />
-      </div>
-      <Typography variant="h4">{info.name}</Typography>
-      <Typography variant="body2">
-        a.k.a.: {info.alternate_names.join(", ")}
-      </Typography>
-      <div className="character_desc">
-        <div className="char_info">
-          <Typography variant="h6">House:</Typography>
-          <Typography variant="body2">{info.house}</Typography>
+        <div className="character_name">
+          <Typography variant="h4">{info.name}</Typography>
+          {!!info.alternate_names.length && (
+            <Typography variant="body2">
+              a.k.a.: {info.alternate_names.join(", ")}
+            </Typography>
+          )}
         </div>
-        <div className="char_info">
-          <Typography variant="h6">Ancestry:</Typography>
-          <Typography variant="body2">{info.ancestry}</Typography>
-        </div>
-        <div className="char_info">
-          <Typography variant="h6">Date Of Birth:</Typography>
-          <Typography variant="body2">{info.dateOfBirth}</Typography>
-        </div>
-        <div className="char_info">
-          <Typography variant="h6">Patronus:</Typography>
-          <Typography variant="body2">{info.patronus}</Typography>
-        </div>
-        <div className="char_info">
-          <Typography variant="h6">Species:</Typography>
-          <Typography variant="body2">{info.species}</Typography>
-        </div>
-        <div className="char_info">
-          <Typography variant="h6">dateOfBirth:</Typography>
-          <Typography variant="body2">{info.dateOfBirth}</Typography>
+        <div className="character_desc">
+          <div className="char_info">
+            <Typography variant="h6">House:</Typography>
+            <Typography variant="body2">{info.house}</Typography>
+          </div>
+          <div className="char_info">
+            <Typography variant="h6">Ancestry:</Typography>
+            <Typography variant="body2">{info.ancestry}</Typography>
+          </div>
+          <div className="char_info">
+            <Typography variant="h6">Date Of Birth:</Typography>
+            <Typography variant="body2">{info.dateOfBirth}</Typography>
+          </div>
+          <div className="char_info">
+            <Typography variant="h6">Patronus:</Typography>
+            <Typography variant="body2">{info.patronus}</Typography>
+          </div>
+          <div className="char_info">
+            <Typography variant="h6">Species:</Typography>
+            <Typography variant="body2">{info.species}</Typography>
+          </div>
         </div>
       </div>
     </div>
